da1716f4256f20ca11344109c82e4c41c9aec694
adsiitam12 <36765493+adsiitam12@users.noreply.github.com>
2018-04-20 10:01:50 -0500

    Update Solucion.java

--- src/main/java/mx/com/itam/adsi/examen/Solucion.java
+++ src/main/java/mx/com/itam/adsi/examen/Solucion.java
@@ -8,7 +8,8 @@ public class Solucion{
     private final static Logger LOG = Logger.getLogger(Solucion.class);   
     /*Sirve porque si existe la subcadena que genera la solucion, cuando duplicamos la cadena original esta debe estar en el centro de la nueva cadena, y al dublicar volemos a escribir la cadena. Quitamos los extremos porque queremos excluir la cadena completa.
     * Por ejemplo, si tenemos  'abab' y la duplcamos, quitando los extremos tenemos 'a bababa b' Y vemos que la cadena original tiene que estar contenida
-    * Esto se debe a que si tenemos que x es la subcadena y la original es xxx, la duplicada sera xxxxxx que necesariamente contiene a la original varias veces, pero de sino genera nunca la contiene.
+    * Esto se debe a que si tenemos que x es la subcadena y la original es xxx, la duplicada sera xxxxxx que necesariamente contiene a la original varias veces, pero de si no genera nunca la contiene.
+    * Mas intuitivamente, si la cadena es una repeticion, solo hace pegarle la subcadena generadora al final para obtenerla de nuevo. Entonces si pegamos la cadena original al final, cubrimos todas nuestras bases sobre la longitud de la cadena generadora
     *La soln. es mas eficiente, pues es tiempo constante y no depende del tamano de la cadena.
     */
    public static boolean gus(String str){
